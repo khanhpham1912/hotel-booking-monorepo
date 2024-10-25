043cf3349f29663b4ef1b83bd5e9ad31e2b7310b
Khanh Pham <khanh.pham@wareflex.io>
2024-10-25 22:48:49 +0700

    .

--- apps/hotel-booking-service/src/main.ts
+++ apps/hotel-booking-service/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Hotel booking API')
+    .addApiKey({ type: 'apiKey', name: 'Authorization' }, 'Authorization')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('/api/docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
